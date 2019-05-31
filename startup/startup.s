8153fc53baff6acb88f7e3c6f51c0999ac7dc136
Hiroaki Fukuda <hiroaki@java2.jp>
2019-05-31 16:23:24 +0900

    add main.c

--- startup/startup.s
+++ startup/startup.s
@@ -1,30 +1,29 @@
 	.section .text
 	.global start
 	.type   start, @function
+	.global reset
+	.type   reset, @function
 
-start:
-	mov r16, r16
-	ldi r20, 0x2
-	ldi r17, 0x1
-	lds r18, 0x40
-	add r18, r17
-	sts 0x40, r18
-	lds r19, 0x40
-	mov r16, r16
+reset:
+	jmp start
+	jmp reset
+	jmp reset
+	jmp reset
 
 ;start:
-;	ldi r16, 0b00100000
-;	out 0x04, r16
-;	ldi r16, 0b00000000
-;	out 0x05, r16
-;	ldi r28, lo8(_bootstack)
-;	ldi r29, hi8(_bootstack)
-;	out 0x3d, r28
-;	out 0x3e, r29
-;	jmp call_main
-;
-;call_main:
-;	rcall main
+;	mov r0, r0
+;	ldi r28, 0x2
+
+start:
+	mov r0, r0
+	ldi r28, lo8(_bootstack)
+	ldi r29, hi8(_bootstack)
+	out 0x3d, r28
+	out 0x3e, r29
+	jmp call_main
+
+call_main:
+	rcall main
 ;
 ;	.globl turnon
 ;	.type  turnon, @function
