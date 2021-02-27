030bbb852e4bae1acbb1f20cf2781c2a512356c8
Hiroaki Fukuda <hiroaki@java2.jp>
2021-02-28 07:55:13 +0900

    remove unnecessary lines

--- led_onoff/main.c
+++ led_onoff/main.c
@@ -1,17 +1,3 @@
-//#include "serial.h"
-
-/*
-int putc(unsigned char c) {
-	if (c == '\n') {
-		serial_send_byte('\r');
-	}
-	return serial_send_byte(c);
-}
-
-unsigned char getc() {
-	return serial_recv_byte();
-}
-*/
 
 void setout();
 void turnon();
@@ -29,23 +15,10 @@ int wait(int seed) {
 }
 
 
-
 int main(void) {
 
-    //	serial_init();
 	setout();
 
-	/*
-	putc('D');
-	char c = getc();
-	turnon();
-	putc(++c);
-	c = getc();
-	getc();
-	c = getc();
-	putc(++c);
-	turnoff();
-	*/
 	turnoff();
 	int flg = 1;
 	int v = 1;
